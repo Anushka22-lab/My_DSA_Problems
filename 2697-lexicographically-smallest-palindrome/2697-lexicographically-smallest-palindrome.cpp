0097617d254f788980dfda78c991a3be4dd07d6f
Anushka22-lab <kesarwanianushka09@gmail.com>
2026-07-28 16:51:19 +0530

    Time: 4 ms (63.23%), Space: 18.6 MB (61.14%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n=s.size();
+      int i=0;
+      int j=n-1;
+      while(i<j){
+       if(s[i]!=s[j] && s[i]>s[j]){
+       s[i]=s[j];
+       }else if(s[i]!=s[j] && s[i]<s[j]){
+        s[j]=s[i];
+       }
+       i++;
+       j--;
+      }
+      return s;
+    } 
+};
